d27df4d0519e99939652ad4b99d9d15445095f77
Katie Hawcutt <katiehawcutt@gmail.com>
2021-11-26 16:47:01 +0000

    improving validation error messages

--- pkg/validator/validator.go
+++ pkg/validator/validator.go
@@ -8,9 +8,9 @@ import (
 
 func ValidateAlbum(album a.AlbumInput) error {
 	return v.ValidateStruct(&album,
-		v.Field(&album.Title, v.Required, v.Length(1, 50).Error("The title must be at least 1 digit")),
-		v.Field(&album.Artist, v.Required, v.Length(1, 50).Error("The artist must be at least 1 digit")),
-		v.Field(&album.Rating, v.Required, v.Min(1), v.Max(5).Error("The rating must be between 1 and 5")),
+		v.Field(&album.Title, v.Required, v.Length(1, 50).Error("The title must be between 1-50 digits")),
+		v.Field(&album.Artist, v.Required, v.Length(1, 50).Error("The artist must be between 1-50 digits")),
+		v.Field(&album.Rating, v.Required, v.Min(1), v.Max(5).Error("The rating must be a number between 1 and 5")),
 	)
 
 }
